80cfce6e704d8623a87870dd182cbfb10889d0a8
ajaymakwana09 <avmakavana677@gmail.com>
2024-10-05 23:10:21 +0530

    server ready 2

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
-require('dotenv').config(); 
+require('dotenv').config(); // Ensure you load your environment variables
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.SERVER);
+        const dbURI = process.env.SERVER; // Get the connection string from the environment variable
+        console.log("Connecting to MongoDB at:", dbURI); // Log the URI for debugging
+        await mongoose.connect(dbURI,);
         console.log("Mongoose connected");
     } catch (error) {
         console.error("Mongoose connection error: ", error);
-        process.exit(1); 
+        process.exit(1); // Exit the application on failure
     }
 };
 
